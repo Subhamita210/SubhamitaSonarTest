759ebed18ffd93d656cef1511fac814f94c65be9
Subhamita210 <subhamita@technoexponent.com>
2022-02-10 12:41:04 +0530

    Project setup

---
+++ sonarTest/src/main/java/com/subhamita/sonarTest/SonarTestApplication.java
@@ -0,0 +1,25 @@
+package com.subhamita.sonarTest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class SonarTestApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(SonarTestApplication.class, args);
+
+		for (int i = 0; i <= 10; i++) {
+			log.info("run");
+//      log.error("""
+//              !!!!!!!!!!!!!!!
+//                   ERROR
+//              !!!!!!!!!!!!!!!
+//              """);
+			log.info("demo");
+		}
+
+	}
+}
